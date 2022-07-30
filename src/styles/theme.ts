b06bbff8a8c56c47eaa8220d37cf37ffb6d5943b
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-07-30 16:15:59 -0300

    Update pallete color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,7 +5,8 @@ export default {
   },
   border: {
     radius: '0.4rem',
-    rounded: '2.4rem'
+    mini: '0.2rem',
+    xxsmall: '0.3rem'
   },
   font: {
     family:
@@ -21,17 +22,34 @@ export default {
       xlarge: '2.0rem',
       xxlarge: '2.8rem',
       huge: '5.2rem'
+    },
+    weights: {
+      light: 300,
+      regular: 400,
+      semiBold: 600,
+      bold: 700
     }
   },
   colors: {
-    primary: '#4B1F37',
-    secondary: '#810121',
-    success: '#6DB437',
-    white: '#F8F7FB',
-    black: '#222222',
-    dark: '#38130B',
-    disabled: '#DBDBDB',
-    gray: {
+    base: {
+      alert: '#DC2626',
+      success: '#22C55E',
+      white: '#F8F7FB',
+      black: '#222222',
+      disabled: '#78716C'
+    },
+    primary: {
+      lighter: '#E8EFF5',
+      light: '#1C5D9F',
+      medium: '#022B54'
+    },
+    secondary: {
+      lighter: '#D1EFF2',
+      light: '#A3DFE6',
+      medium: '#53C3D0',
+      dark: '#34AEBC'
+    },
+    neutral: {
       lightest: '#F4F4F9',
       lighter: '#EFF0F9',
       base: '#E6E6E6',
@@ -62,6 +80,8 @@ export default {
     fast: '0.1s ease-in-out'
   },
   shadow: {
-    inner: 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)'
+    inner: 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)',
+    default: 'rgb(0 0 0 / 15%) 0px 2rem 8rem 0',
+    focus: '0 0 0.5rem #53C3D0'
   }
 } as const
