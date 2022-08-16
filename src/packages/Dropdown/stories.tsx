916551d272070ee3c8581111987b8a3a87793942
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-08-15 22:25:12 -0300

    Create Dropdown component

---
+++ src/packages/Dropdown/stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Dropdown, { DropdownProps } from '.'
+
+export default {
+  title: 'Dropdown',
+  component: Dropdown,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<DropdownProps> = (args) => <Dropdown {...args} />
+
+Default.args = {
+  title: 'Click here',
+  children: 'Content'
+}
