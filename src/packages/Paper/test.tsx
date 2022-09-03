ce75f33f875af54d08e26cb0f21786d141bb3217
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-09-03 15:16:06 -0300

    Add tests to Paper

---
+++ src/packages/Paper/test.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { render, screen } from '../../utils/testUtils'
+import { theme } from '../../styles'
+
+import Paper from '.'
+
+describe('<Paper />', () => {
+  it('Should render with children correctly', () => {
+    render(
+      <Paper placement="bottom">
+        <span>Hello World!</span>
+      </Paper>
+    )
+    const span = screen.getByText(/Hello World!/)
+    expect(span).toBeInTheDocument()
+  })
+  it('Should render in bottom placement', () => {
+    render(
+      <Paper placement="bottom" data-testid="paper">
+        <></>
+      </Paper>
+    )
+    const paper = screen.getByTestId('paper')
+    expect(paper).toHaveStyleRule(
+      'top',
+      `calc(100% + ${theme.spacings.xxsmall})`
+    )
+  })
+  it('Should render in right placement', () => {
+    render(
+      <Paper placement="right" data-testid="paper">
+        <></>
+      </Paper>
+    )
+    const paper = screen.getByTestId('paper')
+    expect(paper).toHaveStyleRule(
+      'left',
+      `calc(100% + ${theme.spacings.xxsmall})`
+    )
+  })
+})
