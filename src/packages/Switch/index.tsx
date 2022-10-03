6bdc7e44271bf03ea05e7fa7d426f2afc9840e8a
njaina <98956159+njaina@users.noreply.github.com>
2022-10-03 16:17:42 +0300

    fix: use prettier

--- src/packages/Switch/index.tsx
+++ src/packages/Switch/index.tsx
@@ -1,21 +1,20 @@
-import React, {useState} from 'react';
-import {Input, Label, Switch} from './Style'
-
+import React, { useState } from "react";
+import { Input, Label, Switch } from "./Style";
 
 export const ToggleSwitch = () => {
-    const [checked, Check] = useState<boolean>(false);
-    const handleChange = () => {
-        if (checked === false) {
-            Check(true);
-        } else {
-            Check(false);
-        }
+  const [checked, Check] = useState < boolean > false;
+  const handleChange = () => {
+    if (checked === false) {
+      Check(true);
+    } else {
+      Check(false);
     }
-    return (
-        <Label>
-            <span>Toggle is {checked ? 'on' : 'off'}</span>
-            <Input checked={checked} type="checkbox" onChange={handleChange}/>
-            <Switch/>
-        </Label>
-    );
+  };
+  return (
+    <Label>
+      <span>Toggle is {checked ? "on" : "off"}</span>
+      <Input checked={checked} type="checkbox" onChange={handleChange} />
+      <Switch />
+    </Label>
+  );
 };
