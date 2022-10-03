3105b9fec13de7669be14eda4052e63749d86703
njaina <98956159+njaina@users.noreply.github.com>
2022-10-03 17:32:57 +0300

    Update index.tsx

--- src/packages/Switch/index.tsx
+++ src/packages/Switch/index.tsx
@@ -1,13 +1,57 @@
 import React, { useState } from 'react'
-import { Input, Label, Switch } from '.'
+import styled from 'styled-components'
+
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
+`
+
+const Switch = styled.div`
+  position: relative;
+  width: 60px;
+  height: 28px;
+  background: #b3b3b3;
+  border-radius: 32px;
+  padding: 4px;
+  transition: 300ms all;
+
+  &:before {
+    transition: 300ms all;
+    content: "";
+    position: absolute;
+    width: 28px;
+    height: 28px;
+    border-radius: 35px;
+    top: 50%;
+    left: 4px;
+    background: white;
+    transform: translate(0, -50%);
+  }
+`
+
+const Input = styled.input`
+  opacity: 0;
+  position: absolute;
+  display: none;
+
+  &:checked + ${Switch} {
+    background: #112ace;
+
+    &:before {
+      transform: translate(32px, -50%);
+    }
+  }
+`
 
 export const ToggleSwitch = () => {
-  const [checked, Check] = useState<boolean>(false)
+  const [checked, Check] = useState<boolean>(false);
   const handleChange = () => {
     if (checked === false) {
-      Check(true)
+      Check(true);
     } else {
-      Check(false)
+      Check(false);
     }
   }
   return (
