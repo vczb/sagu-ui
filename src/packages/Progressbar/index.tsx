dd59e3e6bdbfdee92f64e9de036f79ac4b564b61
CyberBreach <shrivastavapiyush781@gmail.com>
2022-10-04 21:05:35 +0530

    inital commit

---
+++ src/packages/Progressbar/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as S from './styles'
+
+const ProgressBar = (props) => {
+  const { bgcolor, completed } = props;
+  return (
+    <div>
+      <div>
+        <span>{`${completed}%`}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
---
+++ src/packages/Progressbar/styles.ts
@@ -0,0 +1,35 @@
+const ProgressBar = (props) => {
+    const { bgcolor, completed } = props;
+  
+    const containerStyles = {
+      height: 20,
+      width: '100%',
+      backgroundColor: "#e0e0de",
+      borderRadius: 50,
+      margin: 50
+    }
+  
+    const fillerStyles = {
+      height: '100%',
+      width: `${completed}%`,
+      backgroundColor: bgcolor,
+      borderRadius: 'inherit',
+      textAlign: 'right'
+    }
+  
+    const labelStyles = {
+      padding: 5,
+      color: 'white',
+      fontWeight: 'bold'
+    }
+  
+    return (
+      <div style={containerStyles}>
+        <div style={fillerStyles}>
+          <span style={labelStyles}>{`${completed}%`}</span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ProgressBar;
