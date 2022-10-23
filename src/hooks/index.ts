67d6c8b4c36ffa97e83adbd224d0eb9998024b3e
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-10-23 14:42:09 -0300

    Refactor useWindowOnNearBottom hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,3 @@
 export { default as useOnClickOutside } from './click-outside'
 export { default as useOnKeypress } from './key-press'
-export { default as useWindowOnNearBottom } from './use-window-near-bottom'
+export { default as useWindowOnNearBottom } from './window-near-bottom'
