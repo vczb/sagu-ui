9700da9796483e54d7e445231af27729de1b2f69
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-10-06 22:32:14 +0530

    Add new feature: Created Table Section

--- src/packages/Table/style.ts
+++ src/packages/Table/style.ts
@@ -1,23 +1,35 @@
-import styled from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
+import { TableProps } from '.'
 
 export const Table = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  border: 1px solid #ddd;
+  ${({ theme }) => css`
+    width: 100%;
+    border-collapse: collapse;
+    border-width: ${theme.border.xxsmall}
+    border-style: solid;
+    border-color: ${theme.colors.primary.light};
+    `}
 `
 
 export const TableHead = styled.thead`
-  background-color: #f2f2f2;
+  ${({ theme }) => css`
+    background-color: ${theme.colors.base.info};
+  `}
 `
 
 export const TableHeaderCell = styled.th`
-  padding: 10px;
-  text-align: left;
+  ${({ theme }) => css`
+    padding: ${theme.spacings.xxsmall};
+    border: ${theme.border.mini} solid ${theme.colors.primary.dark};
+  `}
 `
 
 export const TableBody = styled.tbody``
 
 export const TableCell = styled.td`
-  padding: 10px;
-  border: 1px solid #ddd;
+  ${({ theme }) => css`
+    padding: ${theme.spacings.xxsmall};
+    border: ${theme.border.mini} solid ${theme.colors.primary.dark};
+    text-align: center;
+  `}
 `
