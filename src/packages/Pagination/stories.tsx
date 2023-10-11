9ff104baa2e518f33c442168cba2ab514ba45a82
Kushal Kumar Nerella <106776313+KushalNerella07@users.noreply.github.com>
2023-10-11 07:06:19 -1000

    Added Pagination Component to Storybook (#104)
    closes https://github.com/vczb/sagu-ui/issues/100

---
+++ src/packages/Pagination/stories.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import Pagination, { PaginationProps } from '.'
+
+export default {
+  title: 'Pagination',
+  component: Pagination,
+  args: {
+    totalCount: 100,
+    currentPage: 1,
+    pageSize: 10,
+    siblingCount: 1,
+    colors: {
+      page: 'gray',
+      hover: 'blue'
+    }
+  }
+}
+
+const Template = (args: PaginationProps) => {
+  const [currentPage, setCurrentPage] = useState(args.currentPage)
+
+  useEffect(() => {
+    setCurrentPage(args.currentPage)
+  }, [args.currentPage])
+
+  return (
+    <Pagination
+      {...args}
+      currentPage={currentPage}
+      onPageChange={(page: number) => {
+        setCurrentPage(page)
+      }}
+    />
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  totalCount: 100,
+  currentPage: 1,
+  pageSize: 10,
+  siblingCount: 1,
+  colors: {
+    page: 'gray',
+    hover: 'blue'
+  }
+}
