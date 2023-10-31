ddb9c4f172556ef1ab2bc762c129fa737a563d27
sowmya-AS <76809502+sowmya-AS@users.noreply.github.com>
2023-10-31 19:33:48 +0530

    test : Adds test for Badge (#125)
    Co-authored-by: sowmya198 <sowmya.ayilam.s@gmail.com>

---
+++ src/packages/Badge/test.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render } from '../../utils/testUtils'
+
+import Badge from '.'
+
+describe('<Badge />', () => {
+  it('should render Badge with default props', () => {
+    const { container } = render(<Badge content="5" />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render Badge with custom size, shape, and color', () => {
+    const { container } = render(
+      <Badge content="New" size="large" shape="circle" color="red" />
+    )
+    expect(container).toMatchSnapshot()
+  })
+  it('should render Badge by handling custom props correctly', () => {
+    const { getByTestId } = render(
+      <Badge data-testid="custom-badge" title="Custom Title" content="Custom" />
+    )
+    const badge = getByTestId('custom-badge')
+    expect(badge).toHaveAttribute('title', 'Custom Title')
+  })
+})
