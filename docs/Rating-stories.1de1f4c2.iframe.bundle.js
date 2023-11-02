509a00e0cde3f782a2761d693c8e4bf09efacb4c
vczb <viniciuszucatti@gmail.com>
2023-11-02 19:36:10 +0000

    Deploying to gh-pages from  @ fb2d4cdfb1d989aad0ad041ccfbc771eb69e8e06 🚀

--- docs/Rating-stories.1de1f4c2.iframe.bundle.js
+++ docs/Rating-stories.1de1f4c2.iframe.bundle.js
@@ -8,7 +8,7 @@
 
     :not(:checked) > label {
       float: right;
-      width: ${theme.font.sizes.xsmall};
+      width: ${theme.font.sizes.medium};
       overflow: hidden;
       white-space: nowrap;
       cursor: pointer;
@@ -32,4 +32,4 @@
     }
   `}
 `;var jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");const Rating=({name,value,onChange})=>{const[selectedValue,setSelectedValue]=(0,react.useState)(value),onOptionChange=event=>{const newValue=+event.target.value;onChange(newValue),setSelectedValue(newValue)};return(0,react.useEffect)((()=>{}),[selectedValue]),(0,react.useEffect)((()=>{setSelectedValue(value)}),[value]),(0,jsx_runtime.jsxs)(RatingContainer,{children:[(0,jsx_runtime.jsx)("input",{type:"radio",id:"star5",name,value:"5",checked:selectedValue>=5,onChange:onOptionChange}),(0,jsx_runtime.jsx)("label",{htmlFor:"star5",title:"5 stars",children:"5 stars"}),(0,jsx_runtime.jsx)("input",{type:"radio",id:"star4",name,value:"4",checked:4===selectedValue,onChange:onOptionChange}),(0,jsx_runtime.jsx)("label",{htmlFor:"star4",title:"4 stars",children:"4 stars"}),(0,jsx_runtime.jsx)("input",{type:"radio",id:"star3",name,value:"3",checked:3===selectedValue,onChange:onOptionChange}),(0,jsx_runtime.jsx)("label",{htmlFor:"star3",title:"3 stars",children:"3 stars"}),(0,jsx_runtime.jsx)("input",{type:"radio",id:"star2",name,value:"2",checked:2===selectedValue,onChange:onOptionChange}),(0,jsx_runtime.jsx)("label",{htmlFor:"star2",title:"2 stars",children:"2 stars"}),(0,jsx_runtime.jsx)("input",{type:"radio",id:"star1",name,value:"1",checked:1===selectedValue,onChange:onOptionChange}),(0,jsx_runtime.jsx)("label",{htmlFor:"star1",title:"1 star",children:"1 star"})]})};Rating.displayName="Rating";const packages_Rating=Rating;try{Rating.displayName="Rating",Rating.__docgenInfo={description:"",displayName:"Rating",props:{name:{defaultValue:null,description:"",name:"name",required:!0,type:{name:"string"}},value:{defaultValue:null,description:"",name:"value",required:!0,type:{name:"number"}},onChange:{defaultValue:null,description:"",name:"onChange",required:!0,type:{name:"(newValue: number) => void"}}}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["src/packages/Rating/index.tsx#Rating"]={docgenInfo:Rating.__docgenInfo,name:"Rating",path:"src/packages/Rating/index.tsx#Rating"})}catch(__react_docgen_typescript_loader_error){}const stories={title:"Rating",component:packages_Rating,args:{name:"Rating",value:1,onChange:newValue=>console.log(newValue)}},Default=args=>(0,jsx_runtime.jsx)(packages_Rating,{...args});Default.displayName="Default"}}]);
-//# sourceMappingURL=Rating-stories.bc915542.iframe.bundle.js.map
+//# sourceMappingURL=Rating-stories.1de1f4c2.iframe.bundle.js.map
