6569ad620fc694d5b8e11e8959ba155a97852fca
Vinicius CZB <viniczb@gmail.com>
2023-11-06 21:01:40 +0100

    Improve Toaster animation

--- src/packages/Toaster/index.tsx
+++ src/packages/Toaster/index.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import Alert, { AlertProps } from '../Alert'
 
+import * as S from './styles'
+import { createPortal } from 'react-dom'
+
 export type ToasterProps = {
   duration?: number | null
   closable?: boolean
+  animated?: 'top' | null
+  isFullWidth?: boolean
 } & Omit<AlertProps, 'header'> // Omit the 'header' prop from AlertProps
 
-const Toaster = ({ duration, closable = true, ...props }: ToasterProps) => {
+const Toaster = ({
+  duration = 5000,
+  closable = true,
+  animated,
+  isFullWidth = false,
+  ...props
+}: ToasterProps) => {
   const [isVisible, setIsVisible] = useState(true)
   useEffect(() => {
     if (typeof duration === 'number') {
@@ -22,6 +33,19 @@ const Toaster = ({ duration, closable = true, ...props }: ToasterProps) => {
 
   if (!isVisible) {
     return null
+  }
+
+  if (animated) {
+    return createPortal(
+      <S.Toaster
+        {...props}
+        closable={closable}
+        animated={animated}
+        duration={duration}
+        isFullWidth={isFullWidth}
+      />,
+      document.querySelector('body')!
+    )
   }
 
   return <Alert {...props} closable={closable} /> // Explicitly set the 'header' prop to null
