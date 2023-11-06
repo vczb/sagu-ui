656e9dada5a7c2c7aa7b16f7a133dd09da6a818c
Vinicius CZB <viniczb@gmail.com>
2023-11-06 09:58:18 +0100

    Refactor Select component

--- src/packages/Select/index.tsx
+++ src/packages/Select/index.tsx
@@ -1,56 +1,28 @@
-import React, { useState, useRef } from 'react'
-import TextField from '../TextField'
-import Paper from '../Paper'
-import { useOnClickOutside, useOnKeypress } from '../../hooks'
+import React, { useMemo } from 'react'
+import { GenericInputProps } from '../GenericInput'
 
 import * as S from './styles'
 
 export type SelectProps = {
-  label: string
-  active?: boolean
-  children: React.ReactNode
-  fieldValue?: string
-  fieldPlaceholder?: string
-}
+  options: string[]
+} & GenericInputProps
 
-const Select = ({
-  label,
-  children,
-  active = false,
-  fieldValue = '',
-  fieldPlaceholder = 'Select...'
-}: SelectProps) => {
-  const [isActive, setIsActive] = useState(active)
-  const ref = useRef<HTMLDivElement>(null)
-
-  useOnClickOutside(ref, () => {
-    setIsActive(false)
-  })
-
-  useOnKeypress('Escape', () => {
-    setIsActive(false)
-  })
+const Select = ({ options, ...props }: SelectProps) => {
+  const selectOptions = useMemo(() => {
+    return options.map((item) => <option key={item}>{item}</option>)
+  }, [options])
 
   return (
-    <S.Select ref={ref}>
-      <S.SelectButton
-        active={isActive}
-        aria-label={`Click to ${isActive ? 'close' : 'open'} Select`}
-        onClick={() => setIsActive(!isActive)}
-      >
-        <TextField
-          label={label}
-          readOnly={true}
-          value={fieldValue}
-          title={fieldValue}
-          placeholder={fieldPlaceholder}
-        />
-      </S.SelectButton>
-
-      <Paper active={isActive} placement="bottom">
-        {children}
-      </Paper>
-    </S.Select>
+    <S.Select
+      {...props}
+      type={undefined}
+      generic={{
+        as: 'select',
+        props: {
+          children: selectOptions
+        }
+      }}
+    />
   )
 }
 
--- src/packages/Select/mock.ts
+++ src/packages/Select/mock.ts
@@ -1,18 +1,1 @@
-export const mock = [
-  {
-    id: 1,
-    name: 'New York'
-  },
-  {
-    id: 2,
-    name: 'Los Angeles'
-  },
-  {
-    id: 3,
-    name: 'Chicago'
-  },
-  {
-    id: 4,
-    name: 'Houston'
-  }
-]
+export const mock = ['New York', 'Los Angeles', 'Chicago', 'Houston']
