5821ab9e4bee2e66fed7a337bf51bdf490a58f5b
vczb <viniciuszucatti@gmail.com>
2024-03-06 13:20:01 +0000

    Deploying to gh-pages from  @ 0c5b76b4cdd589f2ca1d351d7efc6d5ea49ad041 🚀

--- docs/Table-stories.b7e180fb.iframe.bundle.js
+++ docs/Table-stories.b7e180fb.iframe.bundle.js
@@ -8,7 +8,7 @@
     `}
 `,TableHead=styled_components_browser_esm.Ay.thead`
   ${({theme})=>styled_components_browser_esm.AH`
-    background-color: ${theme.colors.base.info};
+    background-color: ${theme.colors.primary.light};
   `}
 `,TableHeaderCell=styled_components_browser_esm.Ay.th`
   ${({theme})=>styled_components_browser_esm.AH`
@@ -23,4 +23,4 @@
     text-align: center;
   `}
 `;var jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");const Table_Table=({columns,data})=>(0,jsx_runtime.jsxs)(Table,{children:[(0,jsx_runtime.jsx)(TableHead,{children:columns.map((column=>(0,jsx_runtime.jsx)(TableHeaderCell,{children:column.title},column.key)))}),(0,jsx_runtime.jsx)(TableBody,{children:data.map(((row,index)=>(0,jsx_runtime.jsx)("tr",{children:columns.map((column=>(0,jsx_runtime.jsx)(TableCell,{children:row[column.key]},column.key)))},index)))})]});Table_Table.displayName="Table";const packages_Table=Table_Table;try{Table_Table.displayName="Table",Table_Table.__docgenInfo={description:"",displayName:"Table",props:{columns:{defaultValue:null,description:"",name:"columns",required:!0,type:{name:"TableColumn[]"}},data:{defaultValue:null,description:"",name:"data",required:!0,type:{name:"TableRow[]"}}}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["src/packages/Table/index.tsx#Table"]={docgenInfo:Table_Table.__docgenInfo,name:"Table",path:"src/packages/Table/index.tsx#Table"})}catch(__react_docgen_typescript_loader_error){}const stories={title:"Table",component:packages_Table},Template=args=>(0,jsx_runtime.jsx)(packages_Table,{...args});Template.displayName="Template";const Default=Template.bind({});Default.args={columns:[{title:"Name",key:"name"},{title:"Age",key:"age"},{title:"Country",key:"country"}],data:[{name:"John",age:30,country:"USA"},{name:"Alice",age:25,country:"Canada"},{name:"Bob",age:35,country:"UK"}]}}}]);
-//# sourceMappingURL=Table-stories.7257f6e1.iframe.bundle.js.map
+//# sourceMappingURL=Table-stories.b7e180fb.iframe.bundle.js.map
